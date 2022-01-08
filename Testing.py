f57a142ecde92a88605deed53777d53fc427073f
adamf <adamfdunmore@gmail.com>
2022-01-08 19:09:13 +0000

    Just wording changes

--- Testing.py
+++ Testing.py
@@ -1,23 +1,26 @@
 import time
+import time
 import keyboard
 import mouse
 
 start = 0
 
-def check_start():
+def switch():
     global start
+    hotkey = input("Enter hotkey")
     while 2 > 1:
 
-        if keyboard.is_pressed("F5") and start == 0:
+        if keyboard.is_pressed("hotkey") and start == 0:
            print("On")
            time.sleep(0.2)
            start = 1
 
-        if keyboard.is_pressed("F5") and start == 1:
+        if keyboard.is_pressed("hotkey") and start == 1:
 
             print("Off")
             time.sleep(0.2)
             start = 0
 
-check_start()
+switch()
+
 
