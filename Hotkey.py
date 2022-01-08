5f854664f538771e60e05f289a9f56e991348424
adamf <adamfdunmore@gmail.com>
2022-01-08 19:09:31 +0000

    A lot of optimisations

--- Hotkey.py
+++ Hotkey.py
@@ -2,76 +2,47 @@ import keyboard
 import mouse
 import time
 
-mork = input("What would you like to use? Mouse or Keyboard ")
+def question():
+    global mork
 
+    morkK = "Keyboard"
+    morkM = "Mouse"
+    mork = input("What would you like to use? Mouse or Keyboard ")
 
-if mork == "Mouse":
+    if mork.lower() == morkM.lower():
+        def mouse():
+            mousestuff = input("Would you like to Right or Left click? ")
+            delay = input("What would you like the delay to be (Number is Seconds)")
 
-    hotkey = input("Enter Hotkey ")
-    end_hotkey = input("Enter ending hotkey ")
-    mousestuff = input("Would you like to Right or Left click? ")
-    delay = input("What would you like the delay to be (Number is Seconds)")
+            delay = int(delay)
 
-    delay = int(delay)
+            if mousestuff == "Right":
+                print("placeholder")
+            if mousestuff == "Left":
+                print("placeholder")
 
-    if mousestuff == "Right":
+    if mork.lower() == morkK.lower():
+        def keyboard():
+                writeY = "Yes"
+                writeN = "No"
+                write = input("Would you like to write something? Yes or No ")
 
-        keyboard.wait(hotkey)
-        time.sleep(1)
+                if write.lower() == writeY.lower():
+                    answer = input("Enter Text ")
+                    autoenter = input("Do you want it to autoenter? Yes or No ")
 
-        while 2 > 1:
-                time.sleep(delay)
-                mouse.right_click()
-                print("Complete")
+                    while 2 > 1:
+                        keyboard.write(answer)
+                        if autoenter == "Yes":
+                            keyboard.press_and_release("enter")
 
-                if keyboard.read_key(end_hotkey):
-                    break
+                if write.lower() == writeN.lower():
+                    answer = input("Enter Button ")
+
+                    while 2 > 1:
+                        keyboard.press_and_release(answer)
 
                 else:
-                    print("Fail")
-
-    if mousestuff == "Left":
-
-        keyboard.wait(hotkey)
-        time.sleep(1)
-
-        while 2 > 1:
-                time.sleep(delay)
-                mouse.click()
-                print("Complete")
-
-                if keyboard.read_key(end_hotkey):
-                    print("break")
-                    break
+                    print("Invalid Input")
 
 
-    else:
-        print("Invalid Input")
-
-if mork == "Keyboard":
-
-    true = input("Would you like to write something? Yes or No ")
-
-    if true == "Yes":
-        hotkey = input("Enter Hotkey ")
-        answer = input("Enter Text ")
-        autoenter = input("Do you want it to autoenter? Yes or No ")
-
-        while 2 > 1:
-            keyboard.wait(hotkey)
-            keyboard.write(answer)
-            if autoenter == "Yes":
-                keyboard.press_and_release("enter")
-                print("Complete")
-    if true == "No":
-        hotkey = input("Enter Hotkey ")
-        answer = input("Enter Button ")
-
-        while 2 > 1:
-            keyboard.wait(hotkey)
-            keyboard.press_and_release(answer)
-            print("Complete")
-    else:
-        print("Invalid Input")
-else:
-    print("Invalid Input")
