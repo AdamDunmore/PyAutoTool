c1b1e14dfebef1616923ada66a1f35fdec42afbd
AdamDunmore <adamfdunmore@gmail.com>
2023-02-04 21:44:58 +0000

    Add hold mouse function

--- AutomationTool.py
+++ AutomationTool.py
@@ -1,9 +1,8 @@
 #Check Lists:
 #   --Mouse Hold Feature
 #   --Recordable Macros
-#   --Error Feedback(In Progress)
-#   --Dropdown keysstill not sure)/(maybe)mouse
-#   --Create key dict
+#   --Themes
+#   --Move error messages
 
 from tkinter import *
 import keyboard
@@ -26,7 +25,7 @@ def stop():
     
 
 class Processes:
-    def __init__(self, delay, key, buttonKey,AE, Mode):    
+    def __init__(self, delay, key, buttonKey,AE, Mode,Hold, HoldTime):    
         self.delay = int(delay)
         self.actKey = key
         self.button = buttonKey
@@ -34,6 +33,8 @@ class Processes:
         self.eventStatus = False
         self.AE = AE
         self.Mode = Mode
+        self.Hold = Hold
+        self.HoldTime = HoldTime
         repeat = True
 
     def onoff(self):
@@ -57,8 +58,15 @@ class Processes:
         
 
     def Mouse_Event(self):
-        if self.status:
+        print(self.Hold)
+        print(self.status)
+        if self.status == True and self.Hold == 0:
             mouse.click(self.button)
+        elif self.status == True and self.Hold == 1:
+            mouse.hold(self.button)
+            window.after(self.HoldTime, lambda: mouse.release(self.button))
+
+
         if repeat == True:
             window.after(self.delay, self.Mouse_Event)
 
@@ -81,6 +89,16 @@ class FrameConstructor:
             self.AEState = self.AE.get()
             print(self.AEState)
 
+    def ToggleMouseHoldTime(self):
+        if self.HoldMouse.get() == 1:
+            self.HoldMouseLabel.place(x=80,y=60)
+            self.HoldMouseLength.place(x=230,y=60)
+
+        elif self.HoldMouse.get() == 0:
+            self.HoldMouseLength.place_forget()
+            self.HoldMouseLabel.place_forget()
+
+
     def __init__(self, keyInput = False, keyInputText = "", AutoEnter = False):
         self.keyInput = bool(keyInput)
         self.keyInputText = keyInputText
@@ -96,7 +114,11 @@ class FrameConstructor:
         '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
         '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+',
         '[', '{', ']', '}', '\\', '|', ';', ':', '\'', '\"', ',', '<', '.', '>', '/', '?',
-        '`', '~'
+        '`', '~', 'control', 'shift', 'alt', 'tab', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'F13', 'F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20', 'delete', 'insert'
+        ]
+
+        self.mouse_keys = [
+        "left", "right", "middle"
         ]
 
         #Construct Frame
@@ -114,23 +136,17 @@ class FrameConstructor:
         self.ADKeyEntry = Entry(self.MyFrame, font=("Segoe UI", 10),fg=ForegroundColour, bg=EntryColour,width=7,highlightcolor=BorderColour)
         self.ADKeyEntry.place(x=240,y=2)
 
-        #Single Key Entry
+        #Key Entry
         if self.keyInput == True:
             self.KeyChoiseLabel = Label(self.MyFrame,font=("Segoe UI",10,"underline"), bg=BackgroundColour,fg=ForegroundColour)
+            self.KeyChoiseEntry = Entry(self.MyFrame,font=("Segoe UI",10), bg=EntryColour,fg=ForegroundColour,width=7,highlightcolor=BorderColour)  
             if self.keyInputText == "Key":
-                self.KeyChoiseLabel.configure(text="Enter Key:")
-                dropdownOption = StringVar()
-                dropdownOption.set("a")
-                self.KeyChoiceDropDown = OptionMenu(self.MyFrame, dropdownOption,"a" ,*self.keys)
-                self.KeyChoiceDropDown.configure(direction="below",bg=BackgroundColour,fg=ForegroundColour,highlightbackground =BorderColour,activebackground=BackgroundColour, activeforeground=ForegroundColour,wraplength=10)
-                self.KeyChoiceDropDown.place(x=75,y=2)
-
+                self.KeyChoiseLabel.configure(text="Enter key:")
             
             elif self.keyInputText == "Keys":
                 self.KeyChoiseLabel.configure(text="Enter Keys:")
-                self.KeyChoiseEntry = Entry(self.MyFrame,font=("Segoe UI",10), bg=EntryColour,fg=ForegroundColour,width=7,highlightcolor=BorderColour)  
-                self.KeyChoiseEntry.place(x=75,y=2)
-            
+                
+            self.KeyChoiseEntry.place(x=75,y=2)
             self.KeyChoiseLabel.place(x=5,y=2)
 
              
@@ -141,9 +157,19 @@ class FrameConstructor:
             Radiobutton(self.MyFrame, variable=self.MouseR,text="Left", value="left",bg=BackgroundColour, activeforeground=EntryColour, activebackground=BackgroundColour,selectcolor=EntryColour, fg=ForegroundColour).place(x=5, y=2)
             Radiobutton(self.MyFrame, variable=self.MouseR,text="Right", value="right",bg=BackgroundColour, activeforeground=EntryColour, activebackground=BackgroundColour,selectcolor=EntryColour, fg=ForegroundColour).place(x=55, y=2,)
 
+            self.HoldMouseLabel = Label(self.MyFrame, text="Hold Time milliseconds:", bg=BackgroundColour, fg=ForegroundColour,font=("Segoe UI", 10,"underline"))
+            self.HoldMouseLength = Entry(self.MyFrame, bg=EntryColour, fg=ForegroundColour,width=10)
+
+            self.HoldMouse = IntVar()
+            self.HoldMouseButton=Checkbutton(self.MyFrame,  text="Hold",foreground=ForegroundColour,background=BackgroundColour,  variable=self.HoldMouse, selectcolor=EntryColour,activebackground=BackgroundColour,activeforeground=ForegroundColour,command=self.ToggleMouseHoldTime)
+            self.HoldMouseButton.place(x=240, y=90)
+
+
+
+
         #Error Label
-        self.ErrorLabel = Label(self.MyFrame, text="", bg=BackgroundColour, fg=ForegroundColour, font=("Segoe UI", 10))
-        self.ErrorLabel.place(x=100,y=80)
+        self.ErrorLabel = Label(self.MyFrame, text="", bg=BackgroundColour, fg=ForegroundColour, font=("Segoe UI", 7),width=20, height=1)
+        self.ErrorLabel.place(x=90,y=70)
 
 
         #Confirm Choise
@@ -190,31 +216,60 @@ class FrameConstructor:
 
 
     def start(self):
-
+        #Error Detection
+        keysString = ""
         if str(self.DelayEntry.get()).isdigit() != True:
             #Tell user to use an int
             self.ErrorLabel.configure(text="You must use a whole number")
  
- 
+        elif str(self.ADKeyEntry.get()) not in self.keys:
+            self.ErrorLabel.configure(text="Please use key from list for activation key")
+
+            for counter in self.keys:
+                keysString += counter + ","
+
+            keyWindow = Tk()
+            keyWindow.title("List")
+            keyWindow.attributes("-topmost",True)
+            Label(keyWindow, text=keysString, wraplength=200).pack()
+
+        elif self.keyInputText == "Key" and str(self.KeyChoiseEntry.get()) not in self.keys:
+            self.ErrorLabel.configure(text="Please use key from list for autokey")
+
+            for counter in self.keys:
+                keysString += counter + ","
+
+            keyWindow = Tk()
+            keyWindow.title("List")
+            keyWindow.attributes("-topmost",True)
+            Label(keyWindow, text=keysString, wraplength=200).pack()
+
+        else:
+            if self.keyInput == True:
+                self.button = self.KeyChoiseEntry.get()
+
+            elif self.keyInput == False:
+                self.button = self.MouseR.get()
+
+                if self.HoldMouse.get() == 0:
+                    self.MouseHoldTime = 0
+
+                else:
+                    self.MouseHoldTime = int(self.HoldMouseLength.get())
 
 
-        if self.keyInput == True:
-            self.button = self.KeyChoiseEntry.get()
+            self.Process = Processes(self.DelayEntry.get(), self.ADKeyEntry.get(), self.button, self.AE.get(),self.keyInputText, self.HoldMouse.get(), self.MouseHoldTime)
+            self.Process.addHotkey()
+            global running
+            running = True
+            self.updateRunning()
 
-        if self.keyInput == False:
-            self.button = self.MouseR.get()
-        self.Process = Processes(self.DelayEntry.get(), self.ADKeyEntry.get(), self.button, self.AE.get(),self.keyInputText)
-        self.Process.addHotkey()
-        global running
-        running = True
-        self.updateRunning()
+            #Removes error message
+            self.ErrorLabel.configure(text="")
 
-        #Removes error message
-        self.ErrorLabel.configure(text="")
-
-        #Toggles Confirm/Stop Button
-        self.Stop.place(x=5, y=80)
-        self.Confirm.place_forget()
+            #Toggles Confirm/Stop Button
+            self.Stop.place(x=5, y=80)
+            self.Confirm.place_forget()
 
 
     def stopRunning(self):
@@ -251,9 +306,5 @@ singlekeyFun()
 Radiobutton(optionsFrame, variable=InitialOptions,text="Single Key", value="Single Key", font=("Segoe UI", 10), bg=BackgroundColour,activebackground=BackgroundColour,fg=ForegroundColour, activeforeground=EntryColour,selectcolor=EntryColour, command=singlekeyFun).place(x=5, y=10)
 Radiobutton(optionsFrame, variable=InitialOptions,text="MultiKey", value="Multi Key",font=("Segoe UI", 10), bg=BackgroundColour,activebackground=BackgroundColour, activeforeground=EntryColour,fg=ForegroundColour,selectcolor=EntryColour, command=multikeyFun).place(x=5, y=50)
 Radiobutton(optionsFrame, variable=InitialOptions,text="Mouse", value="Mouse", font=("Segoe UI", 10), bg=BackgroundColour,activebackground=BackgroundColour, activeforeground=EntryColour,fg=ForegroundColour,selectcolor=EntryColour,command=mouseFun).place(x=5, y=90)
-
-
-
-
 
 window.mainloop()
