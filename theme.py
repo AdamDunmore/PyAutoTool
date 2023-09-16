6bd469c4dfa143353b79c07997d74cd006a0af71
Adam Dunmore <94834543+AdamDunmore@users.noreply.github.com>
2023-09-16 10:35:51 +0100

    Create theme.py

---
+++ theme.py
@@ -0,0 +1,11 @@
+class Theme:
+    def __init__(self) -> None:
+        #Colours
+        self.EntryColour = "#181A21"
+        self.BackgroundColour = "#303443"
+        self.ForegroundColour = "#ffffff"
+        self.BorderColour = "#414554"
+        self.uiFont = "Segoe UI"
+        
+        self.geoWidth = "410"
+        self.geoHeight = "145"
